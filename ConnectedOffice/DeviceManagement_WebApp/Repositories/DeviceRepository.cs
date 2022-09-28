3803f258243082adc3e1738f0fc39f1b8d403d3a
Arthur de Villiers <arthurdevilliers@gmail.com>
2022-09-28 17:06:47 +0200

    Implemented Device CRUD

--- ConnectedOffice/DeviceManagement_WebApp/Repositories/DeviceRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repositories/DeviceRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace DeviceManagement_WebApp.Repositories
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repositories
 {
-    public class DeviceRepository
+    public class DeviceRepository : GenericRepository<Device>, IDeviceRepository
     {
+        protected readonly ConnectedOfficeContext _context;
+        public DeviceRepository(ConnectedOfficeContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public new IEnumerable<Device> GetAll()
+        {
+            var devices = _context.Device
+                .Include(d => d.Category)
+                .Include(d => d.Zone)
+                .ToList();
+
+            return devices;
+        }
+        public new Device GetById(Guid? id)
+        {
+            return _context.Device
+                .Include(d => d.Category)
+                .Include(d => d.Zone)
+                .FirstOrDefault(m => m.DeviceId == id);
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _context.Category.ToList();
+        }
+
+        public IEnumerable<Zone> GetZones()
+        {
+            return _context.Zone.ToList();
+        }
     }
 }
